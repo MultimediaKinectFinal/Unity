a02c6a091cfcce4fbf19fb931670ffca2051ec31
Jason Wang <weitsung0303@gmail.com>
2026-06-11 00:47:13 +0800

    feat: set stop distance

--- Assets/AI/TankData.cs
+++ Assets/AI/TankData.cs
@@ -5,6 +5,6 @@ public class TankData : ScriptableObject
 {
     public float moveSpeed = 0.5f;
     public float fireRate = 2.0f;     // 開火間隔
-    public float attackRange = 2f;   // 偵測距離
+    public float attackRange = 2.0f;   // 偵測距離
     public float maxHP;
 }
